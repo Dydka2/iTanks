d8e6c76b35d3cbaab337c6d505ed2a0c9e94b58e
viking <viking@yandex-team.ru>
2014-08-12 20:21:40 +0400

    Поменял клиентскую структуру

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ var gulp = require('gulp');
 var stylus = require('gulp-stylus');
 
 gulp.task('css', function() {
-    return gulp.src('*.styl')
+    return gulp.src('client/*.styl')
         .pipe(stylus())
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./client/'));
 });
 
 gulp.task('default', ['css']);
 
 gulp.task('watch', ['default'], function() {
-    gulp.watch('*.styl', ['css']);
+    gulp.watch('client/*.styl', ['css']);
 });
